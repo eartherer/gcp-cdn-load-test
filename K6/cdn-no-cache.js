12a4f0264c2a197282f4412ca0bbc94bb3d93c5c
Kittiwat <kosol_kit@hotmail.com>
2023-10-29 00:33:14 +0700

    Add K6 script

---
+++ K6/cdn-no-cache.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+const host = "xxx"
+const port = 8080
+const path = "/simpleconfig"
+
+export const options = {
+    vus: 300,
+    duration: '3m',
+};
+
+export default function () {
+    const res = http.get(`http://${host}:${port}${path}`);
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
